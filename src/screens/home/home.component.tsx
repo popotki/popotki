d4369ec1bda6c73f8e689c9cdbb0c9c04a0a810f
popotki <acquista.alessandro@gmail.com>
2024-01-25 10:10:54 +0100

    change title

--- src/screens/home/home.component.tsx
+++ src/screens/home/home.component.tsx
@@ -1,5 +1,18 @@
 export default function Home(){
     return(
-        <div style={{textAlign:'center', margin:'60px'}}>popotki.com</div>
+        <div className='container'>
+            <div className='avatar'/>
+            <div className='username'>@popotki</div>
+            <div className='post'>
+                Se la gente non facesse qualche volta cose stupide,<br/>
+                niente di intelligente sarebbe mai fatto.
+                <div className='post__title'>[Wittgenstein - il film]</div>
+            </div>
+            <a className='link' href='https://www.instagram.com/popotki/' target='_blank'>instagram</a>
+            <a className='link' href='https://www.facebook.com/popotki/' target='_blank'>facebook</a>
+            <a className='link' href='https://vimeo.com/popotki' target='_blank'>vimeo</a>
+            <a className='link' href='https://youtube.com/@popotki' target='_blank'>youtube</a>
+            <a className='link' href='https://medium.com/@popotki' target='_blank'>medium</a>
+        </div>
     )
 }
